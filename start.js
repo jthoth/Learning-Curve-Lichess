a75284166dac73bee1d1b4d82f06b8d42dace190
Jhon Intriago <jhon.thoth@gmail.com>
2016-10-05 19:12:15 -0500

    _

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 var app = require("./app");
 app.listen(process.env.PORT || 80, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d ", this.address().port);
 });
