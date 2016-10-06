d09c509b9eb8e21651450ef83e54e709b5d62333
Jhon Intriago <jhon.thoth@gmail.com>
2016-10-05 19:19:45 -0500

    Done Changes Succesfull Heroku compiled :+1:

--- start.js
+++ start.js
@@ -1,4 +1,2 @@
 var app = require("./app");
-app.listen(process.env.PORT || 80, function(){
-  console.log("Express server listening on port %d ", this.address().port);
-});
+app.listen(app.get('port'));
